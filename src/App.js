c2e67f9d510a76b5cde05ebd2fa4358245c28326
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-09-27 00:09:06 -0700

    init

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            React deployed using CapRover!
+            React deployed using CapRover by AJ!
           </p>
           <a
             className="App-link"
